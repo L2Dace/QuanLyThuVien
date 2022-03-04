7a36e9fb73c5c0e95cdd69951ab5983e4c3e4de2
Lê Đức Duy <90228749+L2Dace@users.noreply.github.com>
2022-03-04 21:49:32 +0700

    Add files via upload

--- Quản lý thư viện.sql
+++ Quản lý thư viện.sql
@@ -1,9 +1,9 @@
-﻿create database QuanLyThuVien
+﻿create database QuanLyThuVien2
 go
-use QuanLyThuVien
+use QuanLyThuVien2
 go
 create table Sach(
-	maTaiLieu varchar(11) primary key,
+	maTaiLieu char(5) primary key,
 	tenNXB nvarchar(100),
 	soBanPhatHanh int,
 	tenTacGia nvarchar(100),
@@ -11,7 +11,7 @@ create table Sach(
 )
 go
 create table TapChi(
-	maTaiLieu varchar(11) primary key,
+	maTaiLieu char(5) primary key,
 	tenNXB nvarchar(100),
 	soBanPhatHanh int,
 	soPhatHanh int,
@@ -19,7 +19,7 @@ create table TapChi(
 )
 go
 create table Bao(
-	maTaiLieu varchar(11) primary key,
+	maTaiLieu char(5) primary key,
 	tenNXB nvarchar(100),
 	soBanPhatHanh int,
 	ngayPhatHanh date
@@ -27,76 +27,17 @@ create table Bao(
 
 go
 insert into Sach values
-	('S01',N'Kim Đồng',1500,N'Nguyễn Nhật Ánh',85),
-	('S02',N'Tuổi Trẻ',100,N'Phong',50)
+	('SA001',N'Kim Đồng',1500,N'Nguyễn Nhật Ánh',85),
+	('SA002',N'Tuổi Trẻ',100,N'Phong',50)
 
 go
 insert into TapChi values
-	('TC01',N'Đồng Nai',100,2,N'tháng 10'),
-	('TC02','Model',150,8,N'tháng 9')
+	('TC001',N'Đồng Nai',100,2,N'tháng 10'),
+	('TC002','Model',150,8,N'tháng 9')
 
 set dateformat dmy
 go
 insert into Bao values
-	('B01',N'Thanh Niên',165,'06/12/2019'),
-	('B02',N'Dân Trí',100,'15/2/2022')
+	('BA001',N'Thanh Niên',165,'06/12/2019'),
+	('BA002',N'Dân Trí',100,'15/2/2022')
 
-go
-create procedure sach_id
-as
-begin
-declare @ma_next varchar(11)
-declare @max int
-select
-	@max = count(maTaiLieu) + 1 from Sach where maTaiLieu like 'S'
-set
-	@ma_next = 'S' + RIGHT('0' + cast(@max as varchar(9)),9)
-while (exists (select maTaiLieu from Sach where maTaiLieu=@ma_next))
-begin
-	set @max = @max +1
-	set @ma_next = 'S' + RIGHT('0' + cast(@max as varchar(9)),9)
-end
-select @ma_next
-end
---
-go
-create procedure tapChi_id
-as
-begin
-declare @ma_next varchar(11)
-declare @max int
-select
-	@max = count(maTaiLieu) + 1 from TapChi where maTaiLieu like 'TC'
-set
-	@ma_next = 'TC' + RIGHT('0' + cast(@max as varchar(8)),8)
-while (exists (select maTaiLieu from TapChi where maTaiLieu=@ma_next))
-begin
-	set @max = @max +1
-	set @ma_next = 'TC' + RIGHT('0' + cast(@max as varchar(8)),8)
-end
-select @ma_next
-end
---
-go
-create procedure bao_id
-as
-begin
-declare @ma_next varchar(11)
-declare @max int
-select
-	@max = count(maTaiLieu) + 1 from Bao where maTaiLieu like 'B'
-set
-	@ma_next = 'B' + RIGHT('0' + cast(@max as varchar(9)),9)
-while (exists (select maTaiLieu from Bao where maTaiLieu=@ma_next))
-begin
-	set @max = @max +1
-	set @ma_next = 'B' + RIGHT('0' + cast(@max as varchar(9)),9)
-end
-select @ma_next
-end
---
-go
-declare @return_value int
-exec @return_value = dbo.bao_id
-select 'ket qua' = @return_value
-go
